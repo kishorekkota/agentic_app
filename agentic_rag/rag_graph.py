397b65d259b263e330209330b492f2a320bdf274
Kishore Kota <kishorekkota@gmail.com>
2024-10-30 08:32:32 -0500

    updates

---
+++ agentic_rag/rag_graph.py
@@ -0,0 +1,40 @@
+from langgraph.graph import END, StateGraph, START
+from langgraph.prebuilt import ToolNode
+
+# Define a new graph
+workflow = StateGraph(AgentState)
+
+# Define the nodes we will cycle between
+workflow.add_node("agent", agent)  # agent
+retrieve = ToolNode([retriever_tool])
+workflow.add_node("retrieve", retrieve)  # retrieval
+workflow.add_node("rewrite", rewrite)  # Re-writing the question
+workflow.add_node(
+    "generate", generate
+)  # Generating a response after we know the documents are relevant
+# Call agent node to decide to retrieve or not
+workflow.add_edge(START, "agent")
+
+# Decide whether to retrieve
+workflow.add_conditional_edges(
+    "agent",
+    # Assess agent decision
+    tools_condition,
+    {
+        # Translate the condition outputs to nodes in our graph
+        "tools": "retrieve",
+        END: END,
+    },
+)
+
+# Edges taken after the `action` node is called.
+workflow.add_conditional_edges(
+    "retrieve",
+    # Assess agent decision
+    grade_documents,
+)
+workflow.add_edge("generate", END)
+workflow.add_edge("rewrite", "agent")
+
+# Compile
+graph = workflow.compile()
